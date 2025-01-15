1a842d2f7db478650b8bdab3e556146977339e61
Disvster <mmcouto2001@gmail.com>
2025-01-15 20:44:52 +0000

    added up to ex05

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+void	ft_putchar(char x, char y, char z)
+{
+	write(1, &x, 1);
+	write(1, &y, 1);
+	write(1, &z, 1);
+}
+
+void	comma_space(void)
+{
+	write(1, ", ", 2);
+}
+
+void	logic(char x, char y, char z)
+{
+	while (x <= '7')
+	{
+		y = x + 1;
+		while (y <= '8')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				ft_putchar(x, y, z);
+				if (x != '7')
+					comma_space();
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	x;
+	char	y;
+	char	z;
+
+	x = '0';
+	y = x + 1;
+	z = y + 1;
+	logic(x, y, z);
+	write(1, "\n", 1);
+}
